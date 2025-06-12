bbf604e29efcd3a6a082cec6f3434659fa06b401
pamelachalegre <chalegre.pamela@hotmail.com>
2025-06-11 21:33:08 -0300

    ultimo commit

--- hamming.c
+++ hamming.c
@@ -4,13 +4,15 @@
 
 int hamming(const char *a, const char *b) {
     int dist = 0;
+    size_t len_a = strlen(a);
+    size_t len_b = strlen(b);
 
-    if (strlen(a) != strlen(b)) {
+    if (len_a != len_b) {
         printf("Não é possível comparar strings de tamanhos diferentes.\n");
-        return -1;
+        return -1; 
     }
 
-    for (int i = 0; i < strlen(a); i++) {
+    for (int i = 0; i < len_a; i++) {
         if (a[i] != b[i]) {
             dist++;
         }
